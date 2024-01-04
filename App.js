9704a64ab295b711c5cac353eb4750c0ac1979a8
Kiran Madanwad <kiran.madanwad@gmail.com>
2024-01-04 21:56:24 +0530

    EP 02

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
-const parent = React.createElement("div",
-    { id: "parent" },
-    [ React.createElement("div",
-        { id: "child" },
-        [React.createElement("h1", { id: "h11" },
-            "This is h1 tag"),
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+const parent = React.createElement("div",     { id: "parent" },      [React.createElement("div",   { id: "child" },  [React.createElement("h1", { id: "h11" }, "This is h1 tag"),
         React.createElement("h2", { id: "h22" },
             "This is h2 tag")
         ]
-    ), 
+    ),
     React.createElement("div",
         { id: "child2" },
         [React.createElement("h1", { id: "h11" },
             "This is h1 tag"),
         React.createElement("h2", { id: "h22" },
-            "This is h2 tag")
+            "This is h2 tag !!!")
         ]
     )],
 );
-
 
 
 //const heading = React.createElement("h1", {id: "heading"}, "Hello World from React");
